bd188c0802473b551f12f09c3569265dbe078b7d
Devin Heng <hengdevin@gmail.com>
2021-04-21 23:35:37 -0700

    adding the basic code

---
+++ Budgeting App/Budgeting App/Budgeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_AppApp.swift
+//  Budgeting App
+//
+//  Created by Devin Heng on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting App/Budgeting App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Budgeting App
+//
+//  Created by Devin Heng on 4/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
